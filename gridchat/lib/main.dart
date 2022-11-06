58a79c883f023d643e80e744f1aa369e055490da
iecondon <44925871+No1Incorporated@users.noreply.github.com>
2022-11-06 01:30:03 -0500

    Fixed things

--- gridchat/lib/main.dart
+++ gridchat/lib/main.dart
@@ -5,8 +5,8 @@ import 'package:flutter/material.dart';
 import 'package:location/location.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'dart:async';
-
-//import 'location.dart';
+import 'dart:math';
+import 'package:sticky_headers/sticky_headers.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,12 +24,6 @@ class MyApp extends StatelessWidget {
         appBarTheme: const AppBarTheme(
           backgroundColor: Color.fromARGB(255, 43, 43, 43),
           foregroundColor: Color.fromARGB(255, 151, 229, 201),
-          shape: Border(
-          bottom: BorderSide(
-            color: Color.fromARGB(255, 151, 229, 201),
-            width: 2
-          )
-        ),
         ),
       ),
       home: Map(),
@@ -45,66 +39,51 @@ class Message {
   Message.unnamed() : content = 'FUCK';
 }
 
+class Cell {
+  double? latitude;
+  double? longitude;
+  Cell(latitude, longitude) {
+    this.latitude = latitude;
+    this.longitude = longitude;
+  }
+}
+
 class _MapState extends State<Map> {
-  final _suggestions = <Message>[];
-  final _saved = <Message>{};
-  final _biggerFont = const TextStyle(fontSize: 18);
+  Set<Polyline> gridLines = {};
   LocationData? _currentPosition;
   Location location = new Location();
+  var _gridCellCenters = <Cell>[];
   Completer<GoogleMapController> _controller = Completer();
   CameraPosition? _here;
 
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('HLLN'),
-        actions: [
-        ],
-        
-        toolbarHeight: 100,
-        shape: Border(
-          bottom: BorderSide(
-            color: Color.fromARGB(255, 151, 229, 201),
-            width: 2
-          )
+        leading: IconButton(
+            onPressed: _nothing, icon: const Icon(Icons.map_rounded)),
+        title: Image.network(
+          // <-- SEE HERE
+          'https://iili.io/msFVKG.md.png', height: 50,
         ),
-
-        
+        actions: [
+          IconButton(onPressed: _pushChat, icon: const Icon(Icons.chat_bubble)),
+        ],
       ),
-      body: 
-      GoogleMap(
+      body: GoogleMap(
         mapType: MapType.hybrid,
+        polylines: gridLines,
         initialCameraPosition:
-            _here ?? CameraPosition(target: LatLng(30.0, 30.0), zoom: 1),
+            _here ?? CameraPosition(target: LatLng(30.0, 30.0), zoom: .8),
         onMapCreated: (GoogleMapController controller) async {
           _controller.complete(controller);
           final GoogleMapController cntr = await _controller.future;
+          _CreateSpoofGrid();
           location.onLocationChanged.listen((l) {
-            cntr.animateCamera(
-              CameraUpdate.newCameraPosition(
-                CameraPosition(
-                    target: LatLng(l.latitude ?? 30.0, l.longitude ?? 30.0),
-                    zoom: 15),
-              ),
-            );
+            _GetLocalGrid();
           });
         },
         myLocationEnabled: true,
       ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _pushChat,
-        label: Text('Chat with your grid', style: TextStyle(fontWeight: FontWeight.bold),),
-        icon: Icon(Icons.attach_email_outlined),
-        backgroundColor: Color.fromARGB(255, 151, 229, 201),
-        foregroundColor: Color.fromARGB(255, 43, 43, 43),
-        extendedPadding: const EdgeInsets.all(75.0),
-      
-        
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      backgroundColor: Color.fromARGB(255, 43, 43, 43),
-      
-
     );
   }
 
@@ -114,6 +93,7 @@ class _MapState extends State<Map> {
   }
 
   void _pushChat() {
+    print("asdf");
     Navigator.of(context).push(
       MaterialPageRoute(builder: (context) => Chat(), fullscreenDialog: false),
     );
@@ -124,6 +104,60 @@ class _MapState extends State<Map> {
     final GoogleMapController controller = await _controller.future;
     controller.animateCamera(CameraUpdate.newCameraPosition(
         CameraPosition(target: LatLng(30, 30), zoom: 1)));
+  }
+
+  double calculateDistance(lat1, lon1, lat2, lon2) {
+    var p = 0.017453292519943295;
+    var a = 0.5 -
+        cos((lat2 - lat1) * p) / 2 +
+        cos(lat1 * p) * cos(lat2 * p) * (1 - cos((lon2 - lon1) * p)) / 2;
+    return 12.742 * asin(sqrt(a));
+  }
+
+  void _GetLocalGrid() {
+    final Set<Polyline> locGridLines = {};
+    final convert = (1 / 111111) * 50;
+    var counter = 0;
+    for (var x in _gridCellCenters) {
+      var lat = x.latitude ?? 20.0;
+      var long = x.longitude ?? 20.0;
+      var negx = lat - convert;
+      var posx = lat + convert;
+      var negy = long - convert;
+      var posy = long + convert;
+      List<LatLng> latlng = [
+        LatLng(negx, negy),
+        LatLng(posx, negy),
+        LatLng(negx, posy),
+        LatLng(posx, posy),
+        LatLng(posx, negy),
+        LatLng(posx, posy),
+        LatLng(negx, negy),
+        LatLng(negx, posy)
+      ];
+      for (var i = 0; i < 4; i++) {
+        locGridLines.add(Polyline(
+          polylineId: PolylineId((i + 4 * counter).toString()),
+          visible: true,
+          width: 1,
+          points: latlng.sublist(i * 2, i * 2 + 2),
+          color: Colors.red,
+        ));
+      }
+      counter++;
+    }
+    setState(() {
+      gridLines = locGridLines;
+    });
+  }
+
+  _CreateSpoofGrid() {
+    final convert = (1 / 111111);
+    final offset = 5 * convert * 100;
+    for (var i = 0; i < 100; i++) {
+      _gridCellCenters.add(Cell(30 - offset + (i % 10) * convert * 100,
+          30 - offset + (i ~/ 10) * convert * 100));
+    }
   }
 
   fetchLocation() async {
@@ -175,9 +209,10 @@ class Map extends StatefulWidget {
 
 class _ChatState extends State<Chat> {
   final List<String> _suggestions = <String>[
-    "asdfay a ",
-    "a'lf yawom fha9isuf"
+    "Yo knkow, I really think I like cheese",
+    "But I dont"
   ];
+  final myController = TextEditingController();
   final _saved = <Text>{};
   final _biggerFont = const TextStyle(fontSize: 18);
   LocationData? _currentPosition;
@@ -188,41 +223,82 @@ class _ChatState extends State<Chat> {
   // #docregion RWS-build
   @override
   Widget build(BuildContext context) {
-    DateTime now = DateTime.now();
     return Scaffold(
-      // NEW from here ...
-      floatingActionButton: FloatingActionButton(
-        // When the user presses the button, show an alert dialog containing
-        // the text that the user has entered into the text field.
-        onPressed: () {
-          _suggestions.length++;
-            _suggestions.add(myController.text);
-        },
-        child: const Icon(Icons.send),
-        backgroundColor: Color.fromARGB(255, 151, 229, 201),
-        foregroundColor: Color.fromARGB(255, 43, 43, 43),
-      ),
-      backgroundColor: Color.fromARGB(255, 43, 43, 43),
-      appBar: AppBar(
-        title: Image.network(
-          
-          // <-- SEE HERE
-          'https://iili.io/msFVKG.md.png', height: 50,
+        // NEW from here ...
+        floatingActionButton: FloatingActionButton(
+          // When the user presses the button, show an alert dialog containing
+          // the text that the user has entered into the text field.
+          onPressed: () {
+            if (Text(myController.text) != null) {
+              _suggestions.add(myController.text);
+            }
+            ;
+          },
+          child: const Icon(Icons.text_fields),
         ),
-      ),
-      // #docregion itemBuilder
-      body: ListView.builder(
-          padding: const EdgeInsets.all(20.0),
-          itemCount: _suggestions.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Container(
-              
-              height: 30,
-              
-              child: Text(' ${now.hour.toString() + ":" + now.minute.toString() + ":" + now.second.toString() + "      " + _suggestions[index]}', style: TextStyle(color: Color.fromARGB(255, 151, 229, 201))),
-            );
-          }),
-    );
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          leading: IconButton(
+              onPressed: _BackMap, icon: const Icon(Icons.map_rounded)),
+          title: Image.network(
+            // <-- SEE HERE
+            'https://iili.io/msFVKG.md.png', height: 50,
+          ),
+          actions: [
+            IconButton(
+                onPressed: _Refreash, icon: const Icon(Icons.chat_bubble)),
+          ],
+        ),
+        // #docregion itemBuilder
+        // body: ListView(
+        //   children: [
+        //     StickyHeader(
+        //         header: Padding(
+        //           padding: const EdgeInsets.all(16.0),
+        //           child: TextField(
+        //             controller: myController,
+        //           ),
+        //         ),
+        //         content: ListView.builder(
+        //             padding: const EdgeInsets.all(16.0),
+        //             itemCount: _suggestions.length,
+        //             itemBuilder: (BuildContext context, int index) {
+        //               return Container(
+        //                 height: 50,
+        //                 child: Center(child: Text('Test ${_suggestions[index]}')),
+        //               );
+        //             })),
+        //   ],
+        // ),
+
+        body: CustomScrollView(
+          reverse: true,
+          slivers: [
+            SliverPadding(
+                padding: const EdgeInsets.all(20.0),
+                sliver: SliverList(
+                  delegate: SliverChildListDelegate(
+                    <Widget>[
+                      TextField(
+                        controller: myController,
+                      ),
+                    ],
+                  ),
+                )),
+            SliverPadding(
+                padding: const EdgeInsets.all(20.0),
+                sliver: SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                    (BuildContext context, int index) {
+                      return Container(
+                        child: Text('Item: ${_suggestions[index]}'),
+                      );
+                    },
+                    childCount: _suggestions.length,
+                  ),
+                )),
+          ],
+        ));
     // #enddocregion itemBuilder
   }
 
@@ -256,8 +332,6 @@ class _ChatState extends State<Chat> {
   }
   // #enddocregion RWS-build
   // #docregion RWS-var
-
-  final myController = TextEditingController();
 
   @override
   void dispose() {
