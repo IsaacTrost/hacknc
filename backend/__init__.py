0fc9b9b6065ebfca864655200e7d21c56b636e82
cyberseckenny <kennycs@protonmail.com>
2022-11-06 04:20:37 -0500

    Prevent circular import, imported routes

--- backend/__init__.py
+++ backend/__init__.py
@@ -2,7 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-
 db = SQLAlchemy(app)
+
+from backend import routes
+
+
