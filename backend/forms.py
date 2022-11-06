86e245abcc812ac0f9e48eaf8eb1257751411395
cyberseckenny <kennycs@protonmail.com>
2022-11-06 04:06:40 -0500

    Deleted forms and run command

--- backend/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, TextAreaField
-from wtforms.validators import DataRequired
-
-class ChatForm(FlaskForm):
-  content = TextAreaField('Content', validators=[DataRequired()])
-
-
